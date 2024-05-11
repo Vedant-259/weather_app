0b9d20b6c7d6b5619df5bc1bcd45ef7d9c7fd43d
Vedant259 <vedantdubey.123@gmail.com>
2024-05-11 22:00:08 +0530

    initial commit

---
+++ app/utils/defaultStates.tsx
@@ -0,0 +1,39 @@
+const defaultStates = [
+  {
+    name: "London",
+    country: "GB",
+    state: "England",
+    lat: 51.5074,
+    lon: 0.1278,
+  },
+  {
+    name: "Firozabad",
+    country: "IN",
+    state: " Uttar Pradesh",
+    lat:27.1509,
+    lon: 78.3948,
+  },
+  {
+    name: "New York",
+    country: "US",
+    state: "New York",
+    lat: 40.7128,
+    lon: -74.006,
+  },
+  {
+    name: "Bhuj",
+    country: "IN",
+    state: "Gujarat",
+    lat: 23.2531,
+    lon: 69.6693,
+  },
+  {
+    name: "Neemuch",
+    country: "IN",
+    state: "Madhya Pradesh",
+    lat: 24.4757,
+    lon: 74.8741,
+  },
+];
+
+export default defaultStates;
